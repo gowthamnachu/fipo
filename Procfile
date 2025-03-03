351eed58840d42b91e10b269657dba2da645c626
Gowtham Nachu <gowthamnachu545@gmail.com>
2025-03-03 13:53:44 +0530

    4th commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fipo.wsgi --log-file -
--- fipo/settings.py
+++ fipo/settings.py
@@ -25,9 +25,17 @@ LOGIN_URL = '/login/'
 SECRET_KEY = 'django-insecure-p(@)2$^5*or^q_rogi36#n3r^h78h)vy)20fglnun0gp_@)apr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['financeportfolio.herokuapp.com', '.vercel.app', 'localhost', '127.0.0.1']
+
+# Security Settings
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 
 # Application definition
